664c0e7a8c779df933874927ba5b267872947c10
DmitrySmushchenko <d.smushchenko@astoundcommerce.com>
2021-06-17 19:47:32 +0300

    kkkk

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 const msalConfig = {
     auth: {
-      clientId: 'f008a3e2-e4a3-4483-9dea-60c1447bdaff',
-      redirectUri: 'http://localhost:8080'
+      clientId: 'f46c0df1-6b26-4df4-b6cb-9740fc21bc1e',
+      redirectUri: 'https://d-smushchenko.github.io/test-outloo/'
     }
   };
   
